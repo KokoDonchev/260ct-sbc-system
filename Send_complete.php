92118d3582088f34567edb9b14eb294c8ece602e
alex9707 <howkins2@uni.coventry.ac.uk>
2017-04-06 11:55:10 +0100

    Add files via upload

--- Send_complete.php
+++ Send_complete.php
@@ -1,22 +1,37 @@
 <?php
-if(isset($_POST['email'])) {
- 
-    $email_to = "alexanderhowkins@live.co.uk";
-    $email_subject = "Your email subject line";
 
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Comments: ".clean_string($comments)."\n";
+//Provides link back to dashboard
+echo '<a href="dashboard">Back to dashboard</a>
 
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
-?>
- 
-Your message has been sent. We aim to reply to all questions as soon as possible.
- 
-<?php
+';
+
+//These variables are created to access the database booking_systems
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "booking_system";
+
+//The database booking_systems is then accessed using the variables defined above
+$conn = new mysqli($servername, $username, $password, $database);
+
+//The variables firstname, lastname, email and enquiry that were passed from the HTML form are used
+$firstname=$_POST['firstname'];
+$lastname=$_POST['lastname'];
+$email=$_POST['email'];
+$enquiry=$_POST['enquiry'];
+
+//These values are then inserted into the table responses and the variables are added to the corresponding row
+$sql="INSERT INTO responses (Firstname,Surname,Email,Enquiry)
+				VALUES('$firstname','$lastname','$email','$enquiry')";
+
+//An if statement is then used where $conn is the database accessed and adding the information to the database
+//If both are successful the user will be informed and if it isn't the user will be asked to fill in the form again
+if ($conn->query($sql) === TRUE) {
+    echo "Your enquiry has been sent successfully. We aim to be respond as fast as possible.";
+} else {
+    echo "There has been an error with the form submitted. Please try again." . $conn->error;
 }
+
+//It is then closed to finish
+$conn->close();
 ?>
